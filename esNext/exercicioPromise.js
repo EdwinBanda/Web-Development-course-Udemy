c2c6f07acb47ae81e74658b873a2fd5f23df4f89
Edwinb <casemirobanda@gmail.com>
2023-08-01 20:35:36 +0200

    Finalizando o modulo

--- esNext/exercicioPromise.js
+++ esNext/exercicioPromise.js
@@ -6,7 +6,7 @@ const caminho = path.join(__dirname ,'dados.txt')
 function lerArquivo(caminho) {
     return new Promise(resolve => {
         fs.readFile(caminho, function(_, conteudo){
-            resolve(conteudo.toString())
+            resolve(conteudo.toString()) 
         })
     })
 }
