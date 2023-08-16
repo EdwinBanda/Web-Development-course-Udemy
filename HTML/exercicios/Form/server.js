75798f7b601fc08ad46021a2e49da5bae8819d05
Edwinb <casemirobanda@gmail.com>
2023-08-16 18:52:14 +0200

    img

--- HTML/exercicios/Form/server.js
+++ HTML/exercicios/Form/server.js
@@ -1,13 +1,18 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-// const port = 3003
+const port = 3003
 
 app.use(bodyParser.urlencoded({extended: true}))
 
 app.post('/usuarios', (req, res)=>{
     console.log(req.body)
-    res.send('<h1>Parabens</h1>')
+    res.send('<h1>Parabens, usuario incluido</h1>')
+})
+app.post('/usuarios/:id', (req, res)=>{
+    console.log(req.params.id)
+    console.log(req.body)
+    res.send('<h1>Parabens, usuario alterado</h1>')
 })
 
-app.listen(3003)
+app.listen(port)
