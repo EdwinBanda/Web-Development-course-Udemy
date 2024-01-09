38b922527cb81e38f2e680bc545847c6cacc4930
Edwin Banda <casemirobanda@gmail.com>
2024-01-09 16:37:05 +0200

    upload de ficheiro usando xmlHttpRequest

--- ajax/ajax/server.js
+++ ajax/ajax/server.js
@@ -1,11 +1,33 @@
 const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
+const multer = require('multer')
 const port = 8080
 
 app.use(express.static('.'))
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
+
+const storage = multer.diskStorage({
+    destination: function(req, file, callback){
+        callback(null, "./uploads")
+    },
+    fileName: function(req, file, callback){
+        callback(null, `${Date.now()}_${file.originalname}`)
+    },
+})
+
+const upload = multer({ storage }).single('arquivo')
+
+app.post('/upload', (req,res)=>{
+    upload(req, res, err =>{
+        if(err){
+            return res.end("Ocorreu um erro!")
+        }
+        res.end("Concluido com sucesso!")
+    })
+})
+
 
 
 
