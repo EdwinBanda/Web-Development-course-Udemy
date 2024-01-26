5fc8ee487095244ac20e2a2231a84092e0864b74
EdwinBanda <casemirobanda@gmail.com>
2024-01-26 22:15:09 +0200

    SPA project structure

---
+++ gulp/spa/gulpTasks/app.js
@@ -0,0 +1,29 @@
+const gulp = require('gulp');
+
+
+function appHTML(callback){
+
+    return callback()
+}
+
+function appCSS(callback){
+
+    return callback()
+}
+
+function appJS(callback){
+
+    return callback()
+}
+
+function appIMG(callback){
+
+    return callback()
+}
+
+exports.default = {
+    appHTML,
+    appCSS,
+    appJS,
+    appIMG
+}
---
+++ gulp/spa/gulpTasks/deps.js
@@ -0,0 +1,15 @@
+const gulp = require('gulp');
+
+
+function depsCSS(callback){
+    return callback()
+}
+
+function depsFONTS(callback){
+    return callback()
+}
+
+exports.default = {
+    depsCSS,
+    depsFONTS
+}
