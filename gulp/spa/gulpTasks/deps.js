9e0966378fde3cbd53e24cd398d616531a400bd9
EdwinBanda <casemirobanda@gmail.com>
2024-02-03 18:14:06 +0200

    wip: manipulating the module openai to relate with the api

--- gulp/spa/gulpTasks/deps.js
+++ gulp/spa/gulpTasks/deps.js
@@ -1,15 +1,21 @@
 const gulp = require('gulp');
+const uglifycss = require('gulp-uglifycss')
+const concat = require('gulp-concat')
 
 
-function depsCSS(callback){
-    return callback()
+function depsCSS(){
+    return gulp.src('node_modules/font-awesome/css/font-awesome.css')
+        .pipe(uglifycss({"uglyComments": false}))
+        .pipe(concat('deps.min.css'))
+        .pipe(gulp.dest('build/assets/css'))
 }
 
-function depsFONTS(callback){
-    return callback()
+function depsFONTS(){
+    return gulp.src('node_modules/font-awesome/fonts/*.*')
+        .pipe(gulp.dest('build/assets/fonts'))
 }
 
-exports.default = {
+module.exports = {
     depsCSS,
     depsFONTS
 }
--- gulp/spa/gulpTasks/server.js
+++ gulp/spa/gulpTasks/server.js
@@ -1,14 +1,27 @@
 const gulp = require('gulp');
+const webserver = require('gulp-webserver');
+const watch = require('gulp-watch');
+
+function server(){
+    return gulp.src('build')
+        .pipe(webserver({
+            port: 8080,
+            open: true,
+            livereload: true
+        }))
+}
 
 function monitorarArquivos(callback){
+    watch('src/**/*.html', ()=> gulp.series('appHTML')())
+    watch('src/assets/sass/**/*.scss', ()=> gulp.series('appCSS')())
+    watch('src/assets/js/**/*.js', ()=> gulp.series('appJS')())
+    watch('src/assets/imgs/**/*.*', ()=> gulp.series('appIMG')())
+
     return callback()
 }
 
-function server(callback){
-    return callback()
-}
 
-exports.default = {
+module.exports = {
     monitorarArquivos,
     server
 }
